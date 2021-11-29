68b07ec5aad9a632079a8a48f3d20d8bb86a2550
viktorruzaykin <viktorruzaikin@gmail.com>
2021-11-29 16:21:55 +0800

    add lesson_1

---
+++ lesson_1/task_1.py
@@ -0,0 +1,42 @@
+import subprocess
+import ipaddress
+import socket
+import os
+
+
+def ip_address(host):
+    try:
+        if type(host) in (str, int):
+            check = str(ipaddress.ip_address(host))
+        else:
+            return False
+    except ValueError:
+        try:
+            check = socket.gethostbyname(host)
+        except socket.gaierror:
+            return False
+    return check
+
+
+def host_ping(lst):
+    result = []
+    for host in lst:
+        verified_ip = ip_address(host)
+        if verified_ip:
+            with open(os.devnull, 'w') as DNULL:
+                response = subprocess.call(
+                    ["ping", "-n", "2", "-w", "2", verified_ip], stdout=DNULL
+                )
+            if response == 0:
+                result.append(('Доступен', str(host), f'[{verified_ip}]'))
+                continue
+        result.append(('Не доступен', str(host),
+                       f'[{verified_ip if verified_ip else "Не определён"}]'))
+
+    return result
+
+
+ip_addresses = ['127.0.0.1', 'google.ru', 'python.rt', 'python.ru']
+
+for i in host_ping(ip_addresses):
+    print(f'{i[0].ljust(11)} {i[1].ljust(15)} {i[2]}')
